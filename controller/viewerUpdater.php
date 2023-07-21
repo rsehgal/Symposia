11fc9b4ca8cc1f6b048ffb660c32283921db0941
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-07-21 17:15:51 +0530

    Updated ViewUpdater, and update pictures and poster.

--- controller/viewerUpdater.php
+++ controller/viewerUpdater.php
@@ -56,6 +56,7 @@ $table="<table border='1' class='table table-striped'>";
 function GenerateUpdaterView(){
 $obj=new DB();
 $tableName = $_POST["tablename"];
+$prikey= $_POST["prikey"];
 $showUname=1;
 //if($tableName=="refereeList")
 //$showUname=1;
@@ -98,15 +99,16 @@ $table="<table border='1' class='table table-striped'>";
 
                          if($columnName=='uname'){
                         if($showUname==1)
-                                $table.="<td class='".$buttonId."'>" . $row[$columnName] . "</td>";
+                                $table.="<td><input type='text' class='form-control ".$buttonId."' id='".$columnName."' value='".$row[$columnName]."'/>"."</td>";
                         }else{
-                                $table.="<td class='".$buttonId."'>" . $row[$columnName] . "</td>";
+                                //$table.="<td class='".$buttonId."'>" . $row[$columnName] . "</td>";
+                                $table.="<td><input type='text' class='form-control ".$buttonId."' id='".$columnName."' value='".$row[$columnName]."'/>"."</td>";
                         }
                 }
                 //if($allowDeletion==1)
                 if($allowUpdation==1)
                 //$table.="<td><input type='button' class='deleteEntry' oftable='".$tableName."' id='".$row['uname']."' value='Update'></input></td>";
-                $table.="<td><input type='button' class='btn btn-primary updateEntry' oftable='".$tableName."' id='".$buttonId."' value='Update'></input></td>";
+                $table.="<td><input type='button' class='btn btn-primary updateEntry' oftable='".$tableName."' id='".$buttonId."' value='Update' prikey='".$prikey."'></input></td>";
                 //echo 
                 $table.="</tr>";
         }
@@ -115,9 +117,82 @@ $table="<table border='1' class='table table-striped'>";
         $table.="</table><br/>"."ShowUname set <br/>";
         else
         $table.="</table><br/>"."ShowUname NOT set <br/>";*/
-        return $table;
+
+	$associatedJS = '<script>
+
+			
+			$(function(){
+								
+				$(".updateEntry").click(function(e){
+					var tablename=$(this).attr("oftable");
+					var reqClass="."+$(this).attr("id");
+					var data={};
+					$(reqClass).each(function() {
+           			     		console.log($(this).val()); 
+			                	data[$(this).attr("id")]=$(this).val();
+		        		});
+
+					data["function_name"]="UpdateIt";
+					data["prikey"]=$(this).attr("prikey");
+					data["tablename"]=tablename;
+				
+					console.log(data);
+			
+					$.ajax({
+					    url: "../controller/func.php",
+					    method: "POST",
+					    data : data,
+					    success: function(response) {
+					    $("#result").html(response);
+					    }
+					  });
+	
+					//Working
+					/*Object.keys(data).forEach((key) => {
+					  const value = data[key];
+					  console.log(`${key}: ${value}`);
+					});*/
+
+				});
+			});
+			</script>';
+        return $table.$associatedJS;
 
 }
+
+function UpdateIt(){
+	//return "Update It...";
+	//return $_POST["prikey"];
+
+	$prikey = $_POST["prikey"];
+	$tablename = $_POST["tablename"];
+
+	$updateMsg = "";
+	$query = "update ".$tablename." ";
+	$counter=0;
+	foreach ($_POST as $key => $value) {
+	  	//echo "Key: $key, Value: $value" . PHP_EOL;
+		if($key==="prikey" || $key==="function_name" || $key==="tablename"){
+		}else{
+			if($counter===0)
+				$tempQuery = "set $key='$value'";
+			else
+				$tempQuery = ", $key='$value' ";
+			$query.=$tempQuery;
+			$updateMsg.=$key." : ".$value."<br/>";
+			$counter++;
+		}
+	}
+
+	$condition = " where $prikey='".$_POST[$prikey]."'";
+	$query.=$condition;
+
+	$obj = new DB();
+	$result = $obj->GetQueryResult($query);
+	//$result->free();
+	return Message("Data updated sucessfully.","alert-info");
+}
+
 
 function FillDropDown(){
 $obj = new DB();
@@ -125,7 +200,7 @@ $query = "select * from mapping";
 $result = $obj->GetQueryResult($query);
 $dropDownMsg='<h2 class="text-primary">Select a table to view</h2><select class="custom-select" id="tableToView">';
 while($row = $result->fetch_assoc()){
-$dropDownMsg.='<option class="optionTable" tablename="'.$row["tablename"].'">'.$row["taskname"].'</option>';
+$dropDownMsg.='<option class="optionTable" tablename="'.$row["tablename"].'" prikey="'.$row["prikey"].'">'.$row["taskname"].'</option>';
 }
 $dropDownMsg.='</select><br/><hr/>';
 
@@ -148,6 +223,7 @@ $associatedJS='<script>
 			$("#tableToView").change(function(e) {
 				e.preventDefault();
 				data["tablename"]=$(this).children(":selected").attr("tablename");
+				data["prikey"]=$(this).children(":selected").attr("prikey");
 				alert(data["tablename"]);
 				$.ajax({
 				    url: "../controller/func.php",
