16d0fdf687532cb61bcf02386574cedaa1d83262
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-07-25 14:23:41 +0530

    Added Orientation option.

--- view/Forms.php
+++ view/Forms.php
@@ -283,6 +283,16 @@ public function Register($fieldNames){
 					}
 					$selAccReq.='</select></div>';	
 					$formContent.=$selAccReq;
+				}elseif($fieldNames[$i]=="Attending_Orientation"){
+					$options = array('None', 'Yes', 'No'); // The available options
+					$selectedOption=trim($row[$fieldNames[$i]]);
+					$selAccReq= '<select class="form-control accommodation registration" id="Attending_Orientation">';
+					foreach ($options as $option) {
+						$selected = ($option == $selectedOption) ? 'selected' : '';
+						$selAccReq.='<option value="' . $option . '" ' . $selected . '>' . $option . '</option>';
+					}
+					$selAccReq.='</select></div>';	
+					$formContent.=$selAccReq;
 				}
 				elseif($fieldNames[$i]=="Accommodation_Preference"){
 					$options = array('None', 'DAECC Guest House', 'Postgraduate Hostel','Hotel: The Regenza by Tunga','The Jewel Of Chembur'); // The available options
