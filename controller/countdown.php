c3fc6b9faea52ed58452dfa4ef12d9dfd74bc605
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-08-25 17:21:50 +0530

    Trying to put coundown in a elegant manner

---
+++ controller/countdown.php
@@ -0,0 +1,34 @@
+<?php
+
+function CountDown(){
+$countDownMsg = '<div class="bg-secondary text-center text-light">
+			<!-- <div class="row">
+				<div class="col"><h4><hour>Days</hour></h4> </div>
+				<div class="col"><h4><hour>Hours</hour></h4> </div>
+				<div class="col"><h4><hour>Minutes</hour></h4> </div>
+				<div class="col"><h4><hour>Seconds</hour> </h4> </div>
+			</div>
+			-->
+			
+			<div class="container">	
+			   <div  class="row justify-content-center align-items-center text-center">
+				<div class="col-lg-2 col-md-2 col-sm-12 btn-countdown btn-primary " style="text-align: center;">
+					Days : Days</hour></h4> 
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-12 btn-countdown btn-primary " style="text-align: center;">
+					Hours : Hours</hour></h4> 
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-12 btn-countdown btn-primary " style="text-align: center;">
+					Minutes : Minutes</hour></h4> 
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-12 btn-countdown btn-primary " style="text-align: center;">
+					Seconds : Seconds</hour> </h4> 
+				</div>
+			</div>
+			</div>
+		</div>';
+return $countDownMsg;
+}
+
+
+?>
