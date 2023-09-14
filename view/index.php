332809e4c4fd8d30d979f834e4a50f291173cd6c
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-09-14 02:19:21 +0000

    Change font size of message

--- view/index.php
+++ view/index.php
@@ -451,7 +451,7 @@ echo SympnpCarousel();
 
 echo "<div id='headerDiv' class='header'>".SympnpHeader()."</div>";
 echo "<div id='countdown' class='countdown'>".CountDown()."</div>";
-echo "<div id='message' class='blink font-weight-bold text-danger text-center'>
+echo "<div id='message' class='blink font-weight-bold text-danger text-center fs-2'>
 Last date of submission: 15th September
 </div>";
 echo " <div id='countdownn'></div>";
